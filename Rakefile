cf675a691c46d38872bf328e2bc7225fe3b9c32c
Michael Fellinger <m.fellinger@gmail.com>
2009-06-30 12:34:33 +0900

    Better pagination

--- Rakefile
+++ Rakefile
@@ -1,4 +1,7 @@
 require 'rake'
+require 'pathname'
+require 'nokogiri'
+require 'json'
 
 rule(/\.html$/ => [proc{|task_name|
   base = File.basename(task_name, '.html')
@@ -6,3 +9,35 @@ rule(/\.html$/ => [proc{|task_name|
 }]) do |t|
   sh "asciidoc --no-header-footer -o #{t.name} #{t.source}"
 end    
+
+def title_of(name, names)
+  names.empty? ? title : [[name, title], build(names)]
+end
+
+def build(names)
+  title_of(names.shift, names)
+end
+
+task :build do
+  titles = {}
+
+  Pathname.glob 'input/*.txt' do |txt|
+    basename = txt.basename('.txt').to_s
+    html = "output/#{basename}.html"
+
+    Rake::Task[html].invoke
+
+    title = Nokogiri::HTML(File.read(html)).at('h2').inner_html
+    titles[basename] = title
+  end
+  p titles
+
+  File.open('guide', 'w+') do |guide|
+    File.open('order') do |order|
+      order.each_line do |line|
+        line.strip!
+        guide.puts("#{line}\t#{titles[line]}")
+      end
+    end
+  end
+end
--- start.rb
+++ start.rb
@@ -1,4 +1,5 @@
 require 'ramaze'
+require 'compass'
 
 class Presenter < Ramaze::Controller
   map '/'
@@ -6,15 +7,53 @@ class Presenter < Ramaze::Controller
   layout :default
 
   def index(page = 'index')
-    cached("output/#{page}.html")
-  end
+    @page = page
+    file = "output/#{page}.html"
 
-  def cached(file)
-    cache_value(file, :ttl => 60) do
+    cache_value(file, :ttl => 1) do
       system('rake', file)
-      File.read(file)
+      File.read(file) << nav_links
     end
   end
+
+  def list_of_slides
+    [ '<ul>',
+      Dir['input/*.txt'].map{|txt| "<li>#{a(File.basename(txt, '.txt'))}</li>" },
+      '</ul>' ].join
+  end
+
+  def nav_links
+    @guide = Hash[File.readlines('guide').map{|l| l.strip.split("\t") }]
+    @order = File.readlines('order').map{|l| l.strip }
+    @name = action.params.first || 'index'
+
+    <<-HTML
+<div class="nav">
+  <div class="back">#{link_back}</div>
+  <div class="forward">#{link_forward}</div>
+</div>
+  HTML
+  end
+
+  def link_back
+    if back = @order[@order.index(@name) - 1]
+      a @guide[back], back
+    end
+  end
+
+  def link_forward
+    if forward = @order[@order.index(@name) + 1]
+      a @guide[forward], forward
+    end
+  end
+end
+
+class CSS < Ramaze::Controller
+  map '/css'
+  provide :css, :engine => :Sass, :type => 'text/css'
+  trait :sass_options => {
+    :load_paths => Compass::Frameworks::ALL.map{|f| f.stylesheets_directory }
+  }
 end
 
 Ramaze.start
